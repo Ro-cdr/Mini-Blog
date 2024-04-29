49130bc2b438698e8f791adffe1fa1688bffe848
Ro-cdr <rodrigo.sabino3045@gmail.com>
2024-04-29 18:58:40 -0500

    Added delete feature

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import methodOverride from "method-override";
 
 const app = express();
 const port = 3000;
@@ -10,35 +11,54 @@ var isEmpty = true;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method
+      delete req.body._method
+      return method
+    }
+}))
 
 function currentDate() {
     var dt = new Date();
     const months = ["January", "February", "March", "April", "May", "June", 
     "July", "August", "September", "October", "November", "December"];
 
-    var postDate = dt.getDate() + ' ' + months[dt.getMonth()] + ' ' + dt.getFullYear();
-    return postDate;
+    var blogDate = dt.getDate() + ' ' + months[dt.getMonth()] + ' ' + dt.getFullYear();
+    return blogDate;
 }
 
-function Blog(title, content) {
+function Blog(id, title, content) {
+    this.id = id;
     this.title = title;
     this.content = content;
 }
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.render("index.ejs", {today: currentDate(), empty: isEmpty, blogs: blogList});
 })
 
-app.get('/post', (req, res) => {
+app.get('/', (req, res) => {
+    res.redirect('/home');
+})
+
+app.get('/newBlog', (req, res) => {
     res.render("post.ejs");
 })
 
 app.post('/submit', (req, res) => {
-    userBlog = new Blog(req.body["pTitle"], req.body["pContent"]);
+    userBlog = new Blog(blogList.length + 1, req.body["pTitle"], req.body["pContent"]);
     blogList.push(userBlog);
     isEmpty = false;
-    res.redirect("/");
+    res.redirect("/home");
 })
+
+app.delete('/delete', (req, res) => {
+    const blogId = req.body.blogId;
+    blogList = blogList.filter(userBlog => userBlog.id != blogId);
+    res.redirect("/home");
+});
 
 app.listen(port, () => {
     console.log(`Server listening to port: ${port}`);
